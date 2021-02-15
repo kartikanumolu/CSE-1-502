c3cf28a09f62bae5a087cf85c40a3928cf5aa298
kartik <karthikanumolu1225@gmail.com>
2021-02-15 16:36:26 +0530

    2 version

--- project2/p2.c
+++ project2/p2.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 void main()
 {
-int i,n;
+int i,n,count=0;
 printf("enter the number);
 scanf("%d",&n);
 printf("the numbers are");
 if(i=0,i<=n,i++)
 {
 printf("%d\n",i);
+count=count+1;
 }
 }
