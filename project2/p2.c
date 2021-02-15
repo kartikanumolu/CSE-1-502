55b20901c15f2af01b8c687980f5e0cf77019569
kartik <karthikanumolu1225@gmail.com>
2021-02-15 15:56:33 +0530

    first commit

---
+++ project2/p2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,count=0,n;
+printf("enter the value to be caluclated");
+scanf("%d",&n);
+printf("the numbers are");
+if(i=0,i<=n,i++);
+{
+printf("%d \n",i);
+}
+}
+
